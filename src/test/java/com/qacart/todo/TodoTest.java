7cec9ca345d77135d01747b193df3c8382c3d379
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-02-03 18:40:42 +0300

    Add UI steps to automate register testcase

--- src/test/java/com/qacart/todo/TodoTest.java
+++ src/test/java/com/qacart/todo/TodoTest.java
@@ -1,6 +1,8 @@
 package com.qacart.todo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
@@ -19,5 +21,18 @@ public class TodoTest {
     public void should_User_Be_Able_To_Register()
     {
         driver.get("https://todo.qacart.com/signup");
+        WebElement firstName = driver.findElement(By.cssSelector("[data-testid=first-name]"));
+        firstName.sendKeys("Automation");
+        WebElement lastName = driver.findElement(By.cssSelector("[data-testid=last-name]"));
+        lastName.sendKeys("QA");
+        WebElement email = driver.findElement(By.cssSelector("[data-testid=email]"));
+        email.sendKeys("qa@qa.com");
+        WebElement password = driver.findElement(By.cssSelector("[data-testid=password]"));
+        password.sendKeys("qa");
+        WebElement confirmPassword = driver.findElement(By.cssSelector("[data-testid=confirm-password]"));
+        confirmPassword.sendKeys("qa");
+        WebElement submitButton = driver.findElement(By.cssSelector("[data-testid=submit]"));
+        submitButton.click();
+
     }
 }
